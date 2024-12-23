bed2bdf49e36d54b401b029010c11a6ca112a0d6
ShadowCodding <81496704+ShadowCodding@users.noreply.github.com>
2024-12-23 22:12:20 +0100

    A principal commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'images/banners/*.png',
+    'images/icons/*.png'
+}
+
+client_scripts {
+    'client.lua'
+} 
